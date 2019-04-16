bebb2ce5585e295c36082f729dde7af0e8252ddf
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-16 10:46:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 def position_taken?(board,index)
-  if board[index]==" " || board[index]=="" ||board
-"false"
-else "true"
+  if board[index]==" " || board[index]=="" || board[index]==nil
+return false
  end
